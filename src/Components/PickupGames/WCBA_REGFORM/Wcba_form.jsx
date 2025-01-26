27dac833e9aa6454502ffaad27106ee53b3f42cd
Kobe <kobeho23@gmail.com>
2025-01-26 09:13:01 +0800

    Latest commit

--- src/Components/PickupGames/WCBA_REGFORM/Wcba_form.jsx
+++ src/Components/PickupGames/WCBA_REGFORM/Wcba_form.jsx
@@ -136,27 +136,7 @@ const WcbaReg = () => {
           />
         </div>
 
-        <div className="form-group">
-          <label htmlFor="Email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="Contactnum">Contact Number</label>
-          <input
-            type="tel"
-            id="Contactnum"
-            value={contactnum}
-            onChange={(e) => setContactnum(e.target.value)}
-            required
-          />
-        </div>
+       
         <button type="submit" className="signup-button_Wcba" disabled={isDisabled}>
           {isDisabled ? "Registration Unavailable" : "Sign Up"}
         </button>
