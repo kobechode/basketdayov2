35564c3e9e26c41c8d0b02485f7b2cba7c24cb2f
Kobe <kobeho23@gmail.com>
2025-01-31 14:45:38 +0800

    Latest commit

--- src/Firebaseconfig.js
+++ src/Firebaseconfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'; 
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'; 
 import { getFirestore } from "firebase/firestore";
 
 
@@ -24,7 +24,8 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const db = getFirestore(app)
-  export { auth,db};
+  const provider = new GoogleAuthProvider();
+  export { auth,db,provider};
    
  
  
